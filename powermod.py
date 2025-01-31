dd60c3e653ca6844f193952106bf3cdadb261975
Tibelson <165345995+Tibelson@users.noreply.github.com>
2025-01-31 20:06:56 +0000

    Add files via upload

---
+++ powermod.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num = int(input())
+num1 = int(input())
+result = pow(num,num1)
+print(result)
+num2 = int(input())
+result2 = pow(num,num1,num2)
+print(result2)
