174ec19614b64d73ba7bf241c61875916f03dc81
Tibelson <165345995+Tibelson@users.noreply.github.com>
2025-08-20 12:29:01 +0000

    Time: 0 ms (100.00%) | Memory: 21.4 MB (74.77%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+def minNode(node):
+        current = node
+        while current.left:
+            current = current.left
+        return current
+class Solution:
+    
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left,key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right,key)
+
+        else:
+            if not root.left:
+                return root.right
+
+            elif not root.right:
+                return root.left
+
+            else:
+                temp = minNode(root.right)
+                root.val = temp.val
+
+                root.right = self.deleteNode(root.right,temp.val)
+
+
+        return root
+
+        
